7a66c041493b91632b2bde5d0fde54f3a2d1ab9a
Massimiliano Pesce <massimiliano.pesce@gmail.com>
2017-06-05 21:55:04 +0200

    Clean up

--- game/stage_2.py
+++ game/stage_2.py
@@ -12,13 +12,6 @@ class Stage2(Stage):
         self.entities = self.map.all_objects_as_dict('entities')
         self._load_floor_data()
 
-        print(self.entities['player1'].x)
-
-        # self.train_w = 2544
-        # self.train_x = -self.train_w - 10000
-        # self.train_y = 0
-        # self.train_accumulator = 0
-
     def _load_floor_data(self):
         floor = self.map.all_objects_as_list('floor')
         for f in floor:
@@ -33,27 +26,11 @@ class Stage2(Stage):
         return self.map._tmx_data.width
 
     def update(self, game_speed):
-        # self.train_accumulator += game_speed
-        # self.train_x += 40 * game_speed
-        # if self.train_x > self.get_width():
-        #     self.train_x = - self.train_w - 8000
-        #
-        # if self.train_accumulator > 1:
-        #     self.train_y = Utils.random.randint(-1, 1)
-        #     self.train_accumulator = 0
         pass
 
     def draw_background(self, surface, window_x, window_y):
         self.map.draw()
-        # self.map.layer_offsets[0].x = window_x/3
-        # self.map.layer_offsets[1].x = -self.train_x
-        # self.map.layer_offsets[1].y = self.train_y
-        # self.map.layer_offsets[2].x = window_x/2
-        # self.map.layer_offsets[3].x = window_x
-        # self.map.layer_offsets[4].x = window_x
-        # self.map.draw_layers_range(0, 4)
         pass
 
     def draw_foreground(self, surface, window_x, window_y):
-        # self.map.draw_layers_range(4, 1)
         pass
